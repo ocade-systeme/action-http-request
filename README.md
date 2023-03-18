0727a51a1871c7e60203584de000230e34d8d1ef
Valentin Charrier <valentin.charrier@hotmail.fr>
2023-03-18 23:59:14 +0100

    builder

---
+++ README.md
@@ -0,0 +1,9 @@
+# Action Github
+* Cette action permet de lancer une requête HTTP.
+* Pour mémoire, cette action à été créer pour déclencher un build sur un serveur express lorsqu'une release été réalisée.
+
+## Paramètres:
+* **type**: Type de la requête, GET, POST, PUT, DELETE
+* **url**: Url de la requête
+* **headers**: Headers de la requete
+* **body**: Body de la requête
---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+try {
+  // Récupération des inputs
+  const requestType = core.getInput('type', { required: false }) || 'GET';
+  const url = core.getInput('url', { required: true });
+  const headers = JSON.parse(core.getInput('headers', { required: false }) || '{}');
+  const body = JSON.parse(core.getInput('body', { required: false }) || '{}');
+
+  // Configuration de la requête HTTP
+  const config = {
+    method: requestType,
+    url: url,
+    headers: headers,
+    data: body
+  };
+
+  // Envoi de la requête HTTP
+  axios(config).then(function (response) {
+    console.log(JSON.stringify(response.data));
+  }).catch(function (error) {
+    console.log(error);
+  });
+
+} catch (error) {
+  core.setFailed(error.message);
+}
